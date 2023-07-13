fcad4722ddca21f146d210d8597ac2703d74524b
Gigi2303 <ajibola.grace201@gmail.com>
2023-07-13 16:42:02 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+char str[] = "Programming is like building a multilingual puzzle/n";
+
+puts(str);
+
+return (0);
+}
