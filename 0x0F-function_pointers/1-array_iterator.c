7587eb3ac7009e8a4b770a71cef59f1b20c7225c
Gigi2303 <ajibola.grace201@gmail.com>
2023-08-16 20:36:53 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,17 +3,20 @@
 /**
  * array_iterator - prints each array elem on a newl
  * @array: array
- * @size: size to print
- * @action: pointer to the function to use
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int a;
+	unsigned int i;
 
 	if (array == NULL || action == NULL)
 		return;
 
-	for (a = 0, a < size; a++)
-	action(array[a]);
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
+
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,22 +2,21 @@
 /**
  * int_index - return index place if comparison = true, else -1
  * @array: array
- * @size: size of the array
- * @cmp: pointer to function
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
  * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int a;
+	int i;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-	for (a = 0; i < size; a++)
+	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[a]))
-			return (a);
+		if (cmp(array[i]))
+			return (i);
 	}
 	return (-1);
 }
-
