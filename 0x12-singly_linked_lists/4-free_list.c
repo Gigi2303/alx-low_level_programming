d6ce337f0a9751fc985cafc7c7c0476348d18bf6
Gigi2303 <ajibola.grace201@gmail.com>
2023-08-24 20:16:41 -0700

    4. Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,14 +8,14 @@
  */
 void free_list(list_t *head)
 {
-	list_t *tp;
+	list_t *temp;
 
 	while (head)
 	{
-	tp = head->next;
+	temp = head->next;
 	free(head->str);
 	free(head);
-	head = tp;
+	head = temp;
 	}
 }
 
