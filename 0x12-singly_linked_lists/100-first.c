5a7083ca9b2c7e1d691388a3c6adcf44f6f4d007
Gigi2303 <ajibola.grace201@gmail.com>
2023-08-24 20:33:06 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void my_constructor(void);
+
+/**
+ * pre_main_message - This function Prints a message before main function
+ * is executed
+ */
+
+void pre_main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
